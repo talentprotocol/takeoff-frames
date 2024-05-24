d0463de20c1290599f59fe8f79748c1501ad6641
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-05-24 11:01:22 +0200

    update api call

--- app/frames/apply/route.tsx
+++ app/frames/apply/route.tsx
@@ -14,10 +14,11 @@ const handler = frames(async (ctx) => {
       ctx.message?.requesterVerifiedAddresses &&
       ctx.message?.requesterVerifiedAddresses.length > 0
         ? ctx.message?.requesterVerifiedAddresses[0]
-        : ctx.message?.verifiedWalletAddress; // XMTP wallet address
+        : ctx.message?.requesterCustodyAddress ||
+          ctx.message?.verifiedWalletAddress; // XMTP wallet address
 
     if (!userAddress) {
-      throw new Error("User address not found");
+      throw new Error("User not found");
     }
 
     const api_url = process.env.TALENT_API_URL;
@@ -30,9 +31,35 @@ const handler = frames(async (ctx) => {
     };
     const response = await fetch(url, { headers });
     const data = (await response.json()) as any;
-    console.log("passport data", data);
-    if (!response.ok || response.status !== 200 || data.error || !data.passport)
-      throw new Error("Error fetching passport data");
+    if (
+      !response.ok ||
+      response.status !== 200 ||
+      data.error ||
+      !data.passport
+    ) {
+      return {
+        image: (
+          <div tw="relative flex flex-col text-center items-center justify-center">
+            <img
+              src={`${appURL()}/images/frame-application-brussels.png`}
+              tw="w-full"
+            />
+          </div>
+        ),
+        buttons: [
+          <Button
+            action="link"
+            key="1"
+            target="https://passport.talentprotocol.com/signin"
+          >
+            Sign up to passport
+          </Button>,
+        ],
+        imageOptions: {
+          aspectRatio: "1:1",
+        },
+      };
+    }
 
     return {
       image: (
@@ -49,7 +76,7 @@ const handler = frames(async (ctx) => {
           key="1"
           target="https://play.talentprotocol.com/votings/eth-cc"
         >
-          Successfully applied
+          Apply here
         </Button>,
       ],
       imageOptions: {
