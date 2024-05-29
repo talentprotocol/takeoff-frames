ec7c9cd5e73925387b86a050e086659fc5934a81
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-05-29 17:59:59 +0200

    rm 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { FRAMES_BASE_PATH, appURL } from "@/lib/utils";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
